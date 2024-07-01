8863653fee51dbd098de8aa7c3bed6cc02e0d96e
nicolasdumitru <nicolasdumitru13@gmail.com>
2024-07-01 17:49:56 +0300

    Basic & minimal command input.

--- src/command.cpp
+++ src/command.cpp
@@ -1,7 +1,60 @@
 #include <iostream>
+using std::cin;
 #include "command"
 
+inline void command::skipTo(char sep) {
+    char ch = cin.get();
+    while (ch != sep)
+        ch = cin.get();
+}
+
+inline void command::skipToNewline() {
+    skipTo('\n');
+}
+
+inline void command::skipToArgs() {
+    skipTo(' ');
+}
+
+inline void command::readArgs() {
+    args_size = 0;
+    args[args_size] = cin.get();
+    while (args[args_size] != '\n') {
+        args_size++;
+        args[args_size] = cin.get();
+    }
+    args[args_size] = '\0';
+}
+
 void command::readFromStdin() {
-    std::cin.getline(args, 50);
-    std::cout << args;
+    // very minimal Pentobi GTP command processing
+    char ch1, ch2;
+    ch1 = cin.get();
+    switch (ch1) {
+    case 's': // `set_game Blokus Duo`
+        type = set_game;
+        skipToNewline();
+        break;
+    case 'c': // `clear_board` or `cputime`
+        ch2 = cin.get();
+        switch (ch2) {
+        case 'l': type = clear_board; break;
+        case 'p': type = cputime; break;
+        }
+        skipToNewline();
+        break;
+    case 'g': // `genmove [wb]`
+        type = genmove;
+        skipToArgs();
+        readArgs();
+        break;
+    case 'p': // `play [wb] ...`
+        type = play;
+        skipToArgs();
+        readArgs();
+        break;
+    case 'q': // `quit`
+        type = quit;
+        break;
+    }
 }
