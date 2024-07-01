bbfef2b8f77c035d37ebf129a47126e8535fd91f
nicolasdumitru <nicolasdumitru13@gmail.com>
2024-07-01 17:52:00 +0300

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
+#include <iostream>
 #include "command"
 
 int main() {
     command cmd;
+    std::cout << "command line:\n";
     cmd.readFromStdin();
+    std::cout << cmd.type << " args:\n\"" << cmd.args << "\"";
     return 0;
 }
