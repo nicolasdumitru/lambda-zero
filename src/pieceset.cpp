916ebc0851cec15bfb00d8dc34b1916885fddb1a
nicolasdumitru <nicolasdumitru13@gmail.com>
2024-07-04 15:32:18 +0300

    Add Pieceset class.

--- src/pieceset.cpp
+++ src/pieceset.cpp
@@ -1,107 +1,36 @@
-#include "shape.hpp"
+#include "constants.hpp"
+#include "pieceset.hpp"
 
-Shape allShapes[21] = {
-    {
-        "#"
-    },
-    {
-        "##"
-    },
-    {
-        "#",
-        "#",
-        "#"
-    },
-    {
-        "#.",
-        "##"
-    },
-    {
-        "##",
-        "##"
-    },
-    {
-        "##.",
-        ".##"
-    },
-    {
-        "###",
-        ".#."
-    },
-    {
-        "#",
-        "#",
-        "#",
-        "#"
-    },
-    {
-        "#.",
-        "#.",
-        "##"
-    },
-    {
-        "#.#",
-        "###"
-    },
-    {
-        "##",
-        "##",
-        "#."
-    },
-    {
-        ".#",
-        "##",
-        ".#",
-        ".#"
-    },
-    {
-        "###",
-        ".#.",
-        ".#."
-    },
-    {
-        "#",
-        "#",
-        "#",
-        "#",
-        "#",
-    },
-    {
-        ".##",
-        "##.",
-        ".#."
-    },
-    {
-        ".#.",
-        "###",
-        ".#."
-    },
-    {
-        "#..",
-        "##.",
-        ".##"
-    },
-    {
-        ".#",
-        ".#",
-        "##",
-        "#.",
-    },
-    {
-        "##.",
-        ".#.",
-        ".##"
-    },
-    {
-        "#.",
-        "#.",
-        "#.",
-        "##"
-    },
-    {
-        "#..",
-        "#..",
-        "###"
-    },
-    // clang-format on
-};
+void Pieceset::initialize() {
+    for (int i = 0; i < 21; i++)
+        all_shapes[i] = ALL_SHAPES[i];
+}
+
+void Pieceset::generate_rotations() {
+    int n, m, k, i, j;
+    n = 0;
+    for (i = 0; i < 21; i++) {
+        all_rotations[n] = all_shapes[i];
+        m = n;
+        n++;
+        for (j = 0; j < 4; j++) {
+            all_shapes[i].transpose();
+            // clang-format off
+            for (k = m; k < n && all_shapes[i].cmp(all_rotations[k]) == false; k++) {}
+            // clang-format on
+            if (k == n) {
+                all_rotations[n] = all_shapes[i];
+                n++;
+            }
+
+            all_shapes[i].flip_vertically();
+            // clang-format off
+            for (k = m; k < n && all_shapes[i].cmp(all_rotations[k]) == false; k++) {}
+            // clang-format on
+            if (k == n) {
+                all_rotations[n] = all_shapes[i];
+                n++;
+            }
+        }
+    }
+}
--- src/pieceset.hpp
+++ src/pieceset.hpp
@@ -1,4 +1,10 @@
 #pragma once
 
 #include "shape.hpp"
-extern Shape allShapes[21];
+class Pieceset {
+public:
+    Shape all_shapes[21];
+    Shape all_rotations[91];
+    void initialize();
+    void generate_rotations();
+};
