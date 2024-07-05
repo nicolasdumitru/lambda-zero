dcc314cde231b9b24d2d12d7881e14a49c40ea35
nicolasdumitru <nicolasdumitru13@gmail.com>
2024-07-05 11:54:55 +0300

    Add BORDER constant.

--- src/constants.hpp
+++ src/constants.hpp
@@ -1,9 +1,28 @@
 #pragma once
 
 #include "shape.hpp"
+#include <bitset>
+using std::bitset;
 
 const short NUM_SHAPES = 21;
 extern const Shape ALL_SHAPES[NUM_SHAPES];
 
 const short NUM_ROTATIONS = 91;
 const short NUM_PIECES = NUM_ROTATIONS;
+
+const bitset<256> BORDER("1111111111111111"
+                         "1000000000000001"
+                         "1000000000000001"
+                         "1000000000000001"
+                         "1000000000000001"
+                         "1000000000000001"
+                         "1000000000000001"
+                         "1000000000000001"
+                         "1000000000000001"
+                         "1000000000000001"
+                         "1000000000000001"
+                         "1000000000000001"
+                         "1000000000000001"
+                         "1000000000000001"
+                         "1000000000000001"
+                         "1111111111111111");
