04aef35f368721205329dc08799e9bed809dc9d8
nicolasdumitru <nicolasdumitru13@gmail.com>
2024-07-05 10:27:14 +0300

    Change nomenclature and add a useful comment.

--- src/pieceset.cpp
+++ src/pieceset.cpp
@@ -6,29 +6,35 @@ void Pieceset::initialize_shapes() {
         all_shapes[i] = ALL_SHAPES[i];
 }
 
-void Pieceset::generate_rotations() {
+void Pieceset::generate_orientations() {
+    // Core idea:
+    // Transposing a shape, then flipping this vertically and repeating the
+    // process 8 times is a process which incrementally generates all the 8
+    // possible orientations of any piece (rotations, mirrors, flips). Some of
+    // these orientations will be repetitions of others for a subset of the
+    // pieces.
     int n, m, k, i, j;
     n = 0;
     for (i = 0; i < NUM_SHAPES; i++) {
-        all_rotations[n] = all_shapes[i];
+        all_orientations[n] = all_shapes[i];
         m = n;
         n++;
         for (j = 0; j < 4; j++) {
             all_shapes[i].transpose();
             // clang-format off
-            for (k = m; k < n && all_shapes[i].cmp(all_rotations[k]) == false; k++) {}
+            for (k = m; k < n && all_shapes[i].cmp(all_orientations[k]) == false; k++) {}
             // clang-format on
             if (k == n) {
-                all_rotations[n] = all_shapes[i];
+                all_orientations[n] = all_shapes[i];
                 n++;
             }
 
             all_shapes[i].flip_vertically();
             // clang-format off
-            for (k = m; k < n && all_shapes[i].cmp(all_rotations[k]) == false; k++) {}
+            for (k = m; k < n && all_shapes[i].cmp(all_orientations[k]) == false; k++) {}
             // clang-format on
             if (k == n) {
-                all_rotations[n] = all_shapes[i];
+                all_orientations[n] = all_shapes[i];
                 n++;
             }
         }
@@ -40,7 +46,7 @@ void Pieceset::initialize_pieces() {
         for (int row = 0; row < 5; row++) {
             for (int col = 0; col < 5; col++) {
                 bit_pieces[i][5 * row + col]
-                    = (all_rotations[i].shape[row][col] = '#') ? 1 : 0;
+                    = (all_orientations[i].shape[row][col] = '#') ? 1 : 0;
             }
         }
     }
--- src/pieceset.hpp
+++ src/pieceset.hpp
@@ -8,9 +8,9 @@ using std::bitset;
 class Pieceset {
 public:
     Shape all_shapes[NUM_SHAPES];
-    Shape all_rotations[NUM_ROTATIONS];
+    Shape all_orientations[NUM_ROTATIONS];
     bitset<25> bit_pieces[NUM_PIECES];
     void initialize_shapes();
-    void generate_rotations();
+    void generate_orientations();
     void initialize_pieces();
 };
